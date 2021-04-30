fdbf51076eba2f4a4d5c69ed588c5b1204428e95
Seb Moreno <juansebasmorenoruiz@gmail.com>
2021-04-29 23:02:41 -0500

    Testing in Ubuntu

--- back/FileExplorerAPI/views.py
+++ back/FileExplorerAPI/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from subprocess import getoutput as gout
 
 class TestAPI(APIView):
     def get(self, request):
-        return Response({'test': ['pripra']})
+        gout('mkdir public/TestFolder')
+        files = gout('find public/TestFolder/ -maxdepth 1 -type f').split('\n')
+        return Response({'test': ['pripra'], 'dirs': a})
