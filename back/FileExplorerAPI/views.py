ecfe9460e151f49c3781b699923386a6ac8265b7
Sebastián <juansebasmorenoruiz@gmail.com>
2021-05-06 21:20:03 -0500

    API ready

--- back/FileExplorerAPI/views.py
+++ back/FileExplorerAPI/views.py
@@ -7,27 +7,11 @@ from back.settings import BASE_DIR
 
 
 def getActualFolder():
-    return gout('pwd')[len(str(BASE_DIR)):]
+    aux = gout('pwd')[len(str(BASE_DIR)):]
+    return aux if aux else '/'
 
 
-class Actions(APIView):
-    def get(self, request, dtype, name):
-        command = 'mkdir' if dtype == 'dir' else 'touch'
-        return Response(gout(f'{command} {name}'))
-
-    def post(self, request, action):
-        pwd = gout('pwd') + "/"
-        destiny = pwd + request.data['destiny']
-        documents = pwd + f" {pwd}".join(request.data['documents'])
-        return Response(gout(f'{action} {documents} {destiny}'))
-
-    def put(self, request):
-        pwd = gout('pwd') + "/"
-        documents = pwd + f" {pwd}".join(request.data)
-        return Response(gout(f'rm -rf {documents}'))
-
-
-class Info(APIView):
+class DocHandler(APIView):
     def get(self, request):
         content = []
         for i in gout('ls -l').split('\n')[1:]:
@@ -71,10 +55,11 @@ class Info(APIView):
         return Response(gout(f'{command} {name}'))
 
     def patch(self, request):
-        pwd = gout('pwd') + "/"
+        pwd = str(BASE_DIR)
         destiny = pwd + request.data['destiny']
         action = request.data['action']
         documents = pwd + f" {pwd}".join(request.data['documents'])
+        action = 'cp -r' if action == 'copy' else 'mv'
         return Response(gout(f'{action} {documents} {destiny}'))
 
 
@@ -84,19 +69,24 @@ class Permissions(APIView):
 
     def post(self, request):
         name = request.data['name']
+        user = request.data['user']
         permissions = request.data['permissions']
         perms = ''
         for i in ['owner', 'group', 'other']:
             n = "".join(['1' if permissions[i][j] else '0' for j in ['read', 'write', 'exec']])
             perms += str(int(n, 2))
-        return Response(gout(f'chmod -R {perms} {name}'))
+        return Response({
+            'mod': gout(f'chmod -R {perms} {name}'),
+            'own': gout(f'chown -R {user} {name}')
+        })
+
+    def put(self, request):
+        pwd = gout('pwd') + "/"
+        documents = pwd + f" {pwd}".join(request.data['documents'])
+        return Response(gout(f'rm -rf {documents}'))
 
     def patch(self, request):
         pwd = gout('pwd') + "/"
-        documents = pwd + f" {pwd}".join(request.data)
-        return Response(gout(f'rm -rf {documents}'))
-
-    def put(self, request):
-        name = request.data['name']
-        user = request.data['user']
-        return Response(gout(f'chown -R {user} {name}'))
+        old = pwd + request.data['old']
+        updated = pwd + request.data['updated']
+        return Response(gout(f'mv {old} {updated}'))
--- back/back/urls.py
+++ back/back/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # re_path(r'^actions/(?P<action>(mv)|(cp))$', views.Actions.as_view()),
     # re_path(r'^actions/create/(?P<dtype>(dir)|(file))/(?P<name>\w+)$', views.Actions.as_view()),
     # path('actions/delete/', views.Actions.as_view()),
-    path('info', views.Info.as_view()),
+    path('doc-handler', views.DocHandler.as_view()),
     path('permissions', views.Permissions.as_view())
 ]
