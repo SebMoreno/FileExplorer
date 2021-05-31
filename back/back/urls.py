d2ae4f146a12cb3346ec02e5b438fd6930155844
Sebastián <juansebasmorenoruiz@gmail.com>
2021-05-31 01:48:55 -0500

    First Try Production

--- back/back/urls.py
+++ back/back/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
 from FileExplorerAPI import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('doc-handler', views.DocHandler.as_view()),
-    path('permissions', views.Permissions.as_view())
+    path('permissions', views.Permissions.as_view()),
+    re_path(r'^.*', TemplateView.as_view(template_name="../templates/home.html"), name="home")
 ]
